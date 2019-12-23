5df7c2c3281ae3bd941efb1b7f2fe45476e476da
jayfrancis <jpl212011@gmail.com>
2019-12-23 11:26:02 -0500

    form validates

--- finalRestaurant/validate.js
+++ finalRestaurant/validate.js
@@ -1,26 +1,28 @@
 
-function submitForm() {
+//function submitForm() {
     let name = document.getElementById("name")
     let form = document.getElementById("myForm")
-    // let errorElement = document.getElementById("error")
+    let errorElement = document.getElementById("error")
 
-    if(name == "" || !isNaN(name)){
-            alert("Enter your name Nowwwwwwwwww")
-            document.getElementById("name").style.borderColor = "red";
+    // if(name == "" || !isNaN(name)){
+    //         alert("Enter your name Nowwwwwwwwww")
+    //         document.getElementById("name").style.borderColor = "red";
+    //     }
+
+    form.addEventListener("submit", (e) => {
+        let messages = []
+        if(name.value === "" || name.value == null){
+        messages.push("Enter your name Nowwwwwwwwww")
+        document.getElementById("name").style.borderColor = "red";
         }
-
-    // form.addEventListener("submit",(e) => {
-    //     let messages = []
-    //     if(name.value === "" || name.value == null){
-    //     messages.push("Enter your name Nowwwwwwwwww")
-    // }
     
-    //     if(messages.length > 0){
-    //     e.preventDefault()
-    //     errorElement.innerText = messages.join(', ')
-    // }
-    console.log("hello")
+        if(messages.length > 0){
+            e.preventDefault()
+            errorElement.innerText = messages.join(', ')
+        }
+        console.log("hello")
 
-    return false
-}
+    })
+
+
 
